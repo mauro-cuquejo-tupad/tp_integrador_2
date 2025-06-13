215cc1f947f6fd053514ddb2d28c9a3fc24d95fd
Mauro Cuquejo <mauro.cuquejo@gmail.com>
2025-06-13 18:44:59 -0300

    se suben validaciones  conjuntos

--- pruebas-conjuntos.py
+++ pruebas-conjuntos.py
@@ -1,4 +1,4 @@
-from conjuntos import procesar_conjuntos, procesar_conjunto_como_texto, procesar_operacion_conjuntos
+from conjuntos import procesar_conjuntos, procesar_conjunto_como_texto, procesar_operacion_conjuntos, conjunto_a_es_subconjunto_propio_de_c, conjunto_c_es_conjunto_dominante
 # pruebas
 conjuntos = procesar_conjuntos(3, False)
 
@@ -34,3 +34,35 @@ print("Diferencia Simétrica conjuntos:")
 for clave_conjunto in diferencia_simetrica_conjuntos.keys():
     print(
         f"{clave_conjunto} = {{{procesar_conjunto_como_texto(diferencia_simetrica_conjuntos[clave_conjunto])}}}")
+
+
+print("Inicio validaciones expresiones lógicas en lenguaje natural:")
+dnis = {'A': [2, 3, 5, 7, 9], 'B': [0, 1, 3, 4, 8],
+        'C': [0, 2, 3, 4, 5, 7, 9], 'D': [3, 4, 5, 9]}
+
+
+print("validacion 1: Mauro (conjunto D), es subconjunto propio de Diego (conjunto C):")
+mauro_es_subconjunto_de_diego = conjunto_a_es_subconjunto_propio_de_c([
+                                                                      dnis['D'], dnis['C']])
+print("Conjuntos:")
+for clave_conjunto in conjuntos.keys():
+    print(
+        f"{clave_conjunto} = {{{procesar_conjunto_como_texto(conjuntos[clave_conjunto])}}}")
+if mauro_es_subconjunto_de_diego:
+    print("Resultado: Se cumple que Mauro (conjunto D), es subconjunto propio de Diego (conjunto C)")
+else:
+    print("Resultado: NO Se cumple que Mauro (conjunto D), es subconjunto propio de Diego (conjunto C)")
+
+
+print("validacion 2: Diego (conjunto D), es conjunto dominante, respecto de los demás conjuntos:")
+
+diego_es_conjunto_dominante = conjunto_c_es_conjunto_dominante(
+    list(dnis.values()))
+print("Conjuntos:")
+for clave_conjunto in conjuntos.keys():
+    print(
+        f"{clave_conjunto} = {{{procesar_conjunto_como_texto(conjuntos[clave_conjunto])}}}")
+if diego_es_conjunto_dominante:
+    print("Resultado: Se cumple que Diego (conjunto C), es conjunto dominante, respecto de los demás conjuntos")
+else:
+    print("Resultado: NO Se cumple que Diego (conjunto C), es conjunto dominante, respecto de los demás conjuntos")
